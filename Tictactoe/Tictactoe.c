8f3c534ce57d29ff03b25800f4bf29376fa997ff
GanstaKingofSA <azarieldc@gmail.com>
2020-10-20 16:45:17 -0500

    comments and do-while do-while

--- Tictactoe/Tictactoe.c
+++ Tictactoe/Tictactoe.c
@@ -37,12 +37,11 @@
  */
  
 #include <stdio.h>
-/* sets up the tic-tac-toe box length and width */
-char ttt[3][3];
-char player = 'X';
-char cont = 'N';
-int moves = 1;
-int xPos, yPos, digit, win = 0;
+char ttt[3][3]; /* sets the tic-tac-toe box */
+char player = 'X'; /* current player character (X/O) */
+char cont = 'N'; /* continue character for starting a new game */
+int moves = 1; /* moves of the game */
+int xPos, yPos, digit, win = 0; /* position of the row/column, position number and win condition */
 
 /* obtains the integer the user inputs */
 int getValue (void)
@@ -78,43 +77,48 @@ void printBoard (void)
 
 int checkWin(void)
 {
-	int x;
-	int temp = 0;
-	/* column win */
-	for(x = 0; x < 3; x++)
+	int x; /* initializes x for the for loop */
+	int temp = 0; /* sets temp to 0 as if the game wasn't won */
+	for(x = 0; x < 3; x++) /* controls position if different slots for row/column wins */
 	{
+		/* checks if the player got a row, column, diagonal, or reverse-diagonal win */
 		if(ttt[x][0] == player && ttt[x][1] == player && ttt[x][2] == player || ttt[0][x] == player && ttt[1][x] == player && ttt[2][x] == player 
 		|| ttt[0][0] == player && ttt[1][1] == player && ttt[2][2] == player || ttt[0][2] == player && ttt[1][1] == player && ttt[2][0] == player)
 		{
-			temp = 1;
+			temp = 1; /* sets temp to 1 as if the game was won */
 		}
 	}
-	return temp;
+	return temp; /* returns set temp value to win */
 }
 
 void resetArray(void)
 {
 	int x, y;
-	for(x = 0; x < 3; x++)
+	for(x = 0; x < 3; x++) /* loops x position for reset */
 	{
-		for(y = 0; y < 3; y++)
+		for(y = 0; y < 3; y++) /* loops y position for reset */
 		{
-			ttt[x][y] = ' ';
+			ttt[x][y] = ' '; /* makes the row/column given a no character value */
 		}
 	}
-	moves = 1;
+	moves = 1; /* resets moves to 1 */
 }
 
 void main(void)
 {
-	printf("This program re-creates tic-tac-toe in C.\n\n");
+	printf("This program re-creates tic-tac-toe in C.\n");
+	printf("Instructions: Enter a position from 1-9 and try to get 3 X's or O's\nacross, sideways, diagonal or horizontal.\n");
+	/* starts the game and resets if the user 
+	wants to play again */
 	do
 	{
+		/* starts the game by drawing borders, getting a position
+		checks, placement, and increasing movement/switch players */
 		do
 		{
-			printBoard();
-			digit = getValue();
-			switch (digit)
+			printBoard(); /* prints the board */
+			digit = getValue(); /* gets the position for X/O placement */
+			switch (digit) /* switch that outputs the row/column of the position given */
 			{
 				case 1:
 		            xPos = 0;
@@ -153,15 +157,16 @@ void main(void)
 		            yPos = 2;
 		            break;
 			}
+			/* checks if the space is already has a X or O */
 			if(ttt[xPos][yPos] == 'X' || ttt[xPos][yPos] == 'O')
 			{
-				printf("A player already occupies this slot. Try again.\n");
+				printf("A player already occupies this slot. Try again.\n"); /* returns that the spot is occupied */
 			}
 			else
 			{
-				ttt[xPos][yPos] = player;
-				win = checkWin();
-				if (win == 0)
+				ttt[xPos][yPos] = player; /* sets row/column to the player's char symbol */
+				win = checkWin(); /* check if the player won based off tic-tac-toe win rules */
+				if (win == 0) /* if no one has yet won, switch player symbols */
 				{	
 					if (player == 'X')
 					{
@@ -171,25 +176,25 @@ void main(void)
 					{
 						player = 'X';
 					}
-					moves++;
+					moves++; /* increase movement turns over time */
 				}
 				else
 				{
-					moves = 10;
+					moves = 10; /* make moves 10 to end the do-while */
 				}
 			}
 			
-		} while (moves < 10);
-		if (win == 1)
+		} while (moves < 10); /* keep doing the do until moves is 10 */
+		if (win == 1) /* checks if the game is won */
 		{
-			printf("Player %c wins!", player);
+			printf("Player %c wins!", player); /* outputs current player won the game */
 		}
 		else
 		{
-			printf("A tie happened. No one wins.");
+			printf("A tie happened. No one wins."); /* outputs a tie if there is no winner */
 		}
-		resetArray();
-		printf("\n\nDo you want to play again? (Y/N): ");
-		scanf(" %c", &cont);
-	} while(cont == 'Y' || cont == 'y');
+		resetArray(); /* resets the char array and move count for a new game */
+		printf("\n\nDo you want to play again? (Y/N): "); /* asks if the user wants to play again */
+		scanf(" %c", &cont); /* returns input as cont */
+	} while(cont == 'Y' || cont == 'y'); /* continue do until cont is not Y or y */
 }
