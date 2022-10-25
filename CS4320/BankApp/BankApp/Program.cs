5ed9ecc1b09f1c8b35609e63ed37b259d141cd41
GanstaKingofSA <azarieldc@gmail.com>
2022-10-25 09:52:30 -0500

    submit hw

--- CS4320/BankApp/BankApp/Program.cs
+++ CS4320/BankApp/BankApp/Program.cs
@@ -136,6 +136,78 @@ namespace BankApp
             }
         }
 
+        static Account isMin(Account current, Account loopCurrent)
+        {
+            if (current == null)
+            {
+                return loopCurrent;
+            }
+            else if (current.Balance > loopCurrent.Balance)
+            {
+                return loopCurrent;
+            }
+            else
+            {
+                return current;
+            }
+        }
+        static Account isMax(Account current, Account loopCurrent)
+        {
+            if (current == null)
+            {
+                return loopCurrent;
+            }
+            else if (current.Balance < loopCurrent.Balance)
+            {
+                return loopCurrent;
+            }
+            else
+            {
+                return current;
+            }
+        }
+
+        static void DisplayMinMaxTotal()
+        {
+            decimal total = 0;
+            Account minAcct = null;
+            Account maxAcct = null;
+
+            for (int i = 0; i < accountsList.Count; i++)
+            {
+                total += accountsList[i].Balance;
+                minAcct = isMin(minAcct, accountsList[i]);
+                maxAcct = isMax(maxAcct, accountsList[i]);
+            }
+
+            Console.WriteLine("\nTotal Balance: ${0}.", total);
+            Console.WriteLine("Account with Minimum Balance: {0} ({1}) - ${2}.", minAcct.HolderName, minAcct.AccountNo, minAcct.Balance);
+            Console.WriteLine("Account with Maximum Balance: {0} ({1}) - ${2}.\n", maxAcct.HolderName, maxAcct.AccountNo, maxAcct.Balance);
+        }
+
+        static void SaveMinMaxTotal(string FilePath)
+        {
+            decimal total = 0;
+            Account minAcct = null;
+            Account maxAcct = null;
+
+            for (int i = 0; i < accountsList.Count; i++)
+            {
+                total += accountsList[i].Balance;
+                minAcct = isMin(minAcct, accountsList[i]);
+                maxAcct = isMax(maxAcct, accountsList[i]);
+            }
+
+            StreamWriter fileWriter = new StreamWriter(FilePath);
+
+            fileWriter.WriteLine("Total Balance: ${0}", total);
+            fileWriter.WriteLine("Account with Minimum Balance: {0} ({1}) - ${2}.", minAcct.HolderName, minAcct.AccountNo, minAcct.Balance);
+            fileWriter.WriteLine("Account with Maximum Balance: {0} ({1}) - ${2}.", maxAcct.HolderName, maxAcct.AccountNo, maxAcct.Balance);
+
+            fileWriter.Close();
+            Console.WriteLine("\nMin Max Total File has been saved.");
+        }
+
         static int Menu()
         {
             int choice;
@@ -147,7 +219,9 @@ namespace BankApp
             Console.WriteLine("5-Delete account");
             Console.WriteLine("6-Edit account");
             Console.WriteLine("7-Save to File");
-            Console.WriteLine("8-Exit");
+            Console.WriteLine("8-Compute Total, Maximum and Minimum Balances");
+            Console.WriteLine("9-Save Total, Maximum and Minimum Balances to File");
+            Console.WriteLine("10-Exit");
 
             Console.Write("Choice > ");
             choice = int.Parse(Console.ReadLine());
@@ -158,7 +232,7 @@ namespace BankApp
         static void Main(string[] args)
         {
             int option;
-            while((option = Menu()) != 8)
+            while((option = Menu()) != 10)
             {
                 switch(option)
                 {
@@ -182,6 +256,12 @@ namespace BankApp
                         break;
                     case 7:
                         SaveXMLFile("Accounts.xml");
+                        break;
+                    case 8:
+                        DisplayMinMaxTotal();
+                        break;
+                    case 9:
+                        SaveMinMaxTotal("MinMaxTotal.txt");
                         break;
                     default:
                         Console.WriteLine("Invalid input.");
